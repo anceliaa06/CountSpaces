e4f823cacc6d2a98f42da4bc5e0f2cbaf26abb00
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-16 22:36:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CountSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+
+            int spaceCount = 0;
+
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+
+            Console.WriteLine("Total spaces: " + spaceCount);
+        }
+    }
+}
